aaa241ede8d644de0cea20fa3f46eecf1d30479a
Vaishali0123 <95864468+Vaishali0123@users.noreply.github.com>
2024-03-23 14:30:56 +0530

    12

--- app/lwozxip/component.jsx
+++ app/lwozxip/component.jsx
@@ -2,11 +2,14 @@
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+import Loading from "../assests/loading.json";
 
 const Component = () => {
   const searchparams = useSearchParams();
   const id = searchparams.get("id");
-  const temp = searchparams.get("temp")
+  const temp = searchparams.get("temp");
   const router = useRouter();
 
   const fetchid = async () => {
@@ -18,7 +21,7 @@ const Component = () => {
       if (res.data.success) {
         const data = res.data.user;
         sessionStorage.setItem("data", JSON.stringify(data));
-        sessionStorage.setItem("temp", Number(temp))
+        sessionStorage.setItem("temp", Number(temp));
         router.push("/");
       } else {
         console.log("error");
@@ -36,8 +39,10 @@ const Component = () => {
 
   return (
     <>
-      <div>
-
+      <div className="fixed inset-0 w-screen flex justify-center items-center dark:bg-[#273142] bg-white h-screen">
+        <div>
+          <Lottie animationData={Loading} loop size={250} />
+        </div>
       </div>
     </>
   );
